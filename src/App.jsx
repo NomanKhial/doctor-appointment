f628b641b3b04a3cdaa4d8bc12579cf317f263d4
Noman <nomankhialb@gmail.com>
2025-04-09 15:01:39 +0500

    initial setup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h2>hello</h2>
-    </>
+    <div className='border'>
+      hi
+    </div>
   )
 }
 
---
+++ src/pages/Apoinments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Apoinments() {
+    return (
+        <div>
+            Apoinments
+        </div>
+    )
+}
+
+export default Apoinments
