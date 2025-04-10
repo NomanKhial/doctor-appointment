9fbc92130125f12ebaea69e01ef230de9900511e
Noman <nomankhialb@gmail.com>
2025-04-10 16:09:26 +0500

    front-end development has  started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from "./pages/Doctors"
+import Profile from "./pages/Profile"
 
 function App() {
   return (
-    <div className='border'>
-      hi
+    <div className='mx-4 sm:mx-[10%]'>
+
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
     </div>
   )
 }
